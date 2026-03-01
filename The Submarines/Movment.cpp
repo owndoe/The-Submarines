3acc96f34b95f96c4d4e877c6d3d2cd8c7cc24fd
owndoe <milipad1234@gmail.com>
2026-03-01 13:52:25 +0900

    3d

--- The Submarines/Movment.cpp
+++ The Submarines/Movment.cpp
@@ -1,2 +1,7 @@
 #include "Status.h"
 
+void Submarine::Move(int dx, int dy) {
+	pos.x += dx;
+	pos.y += dy;
+	battery -= dx + dy;
+}
--- The Submarines/Status.h
+++ The Submarines/Status.h
@@ -7,17 +7,20 @@ struct Point {
 };
 
 class Submarine {
-public:
+private:
     Point pos;
     int battery;
     int oxygen;
 
+public:
     Submarine() :pos({ 0,0 }), battery(100), oxygen(100) {}
-};
 
-void ShowStatus(Submarine sub) {
-    std::cout << "--- [ 시스템 상태 ] ---" << std::endl;
-    std::cout << "좌표: (" << sub.pos.x << ", " << sub.pos.y << ")" << std::endl;
-    std::cout << "전력: " << sub.battery << "% | 산소: " << sub.oxygen << "%" << std::endl;
-    std::cout << "-----------------------" << std::endl;
-}
+    void ShowStatus() {
+        std::cout << "--- [ 시스템 상태 ] ---" << std::endl;
+        std::cout << "좌표: (" << this->pos.x << ", " << this->pos.y << ")" << std::endl;
+        std::cout << "전력: " << this->battery << "% | 산소: " << this->oxygen << "%" << std::endl;
+        std::cout << "-----------------------" << std::endl;
+    }
+
+    void Move(int dx, int dy);
+};
